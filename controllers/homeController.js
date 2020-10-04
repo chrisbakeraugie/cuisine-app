440717d1191e744657cd13c10ba5260e9b60ea98
Chris Baker <chrisbaker@Chriss-MBP.home>
2020-10-04 17:32:12 -0400

    Created routes, controllers, ejs layout, views, and error messaging for Mock Up Cuisine website. Functioning app without persistance (Mongo/database connections)

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,12 +1,28 @@
+// A little courses placeholder (to mock-up coruses view)
+var courses = [
+  {
+    title: 'Event Driven Cakes',
+    cost: 50
+  },
+  {
+    title: 'Asynchronous Artichoke',
+    cost: 25
+  },
+  {
+    title: 'Object Oriented Orange Juice',
+    cost: 10
+  }
+];
 
+// !!! Express.js will find the views you are referencing in the render statement
 exports.showCourses = (req, res) => {
-  res.render('courses');
+  res.render('courses', {
+    offeredCourses: courses
+  });
 }
-
 exports.showSignUp = (req,res) => {
   res.render('contact');
 }
-
 exports.postedSignUpForm = (req,res) => {
-  res.redner('thanks');
+  res.render('thanks');
 }
--- main.js
+++ main.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const app = express();
 const homeController = require('./controllers/homeController');
+const errorController = require('./controllers/errorController');
+const layouts =  require('express-ejs-layouts');
+
 
 app.set('port', process.env.PORT || 3000);
+
+app.set('view engine', 'ejs'); // Set app to use ejs
+app.use(layouts); // Set the app to use the layout
 
 
 /**
@@ -19,6 +25,10 @@ app.get('/', (req,res) => {
 app.get('/courses', homeController.showCourses);
 app.get('/contact', homeController.showSignUp);
 app.post('/contact', homeController.postedSignUpForm);
+
+// Errors need to be last routes - act as a catch all for your website
+app.use(errorController.pageNotFoundError);
+app.use(errorController.internalServerError);
 
 app.listen(app.get("port"), () => {
   console.log(
