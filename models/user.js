94cf54db0558050521da3789f8fcefc846ce02ce
Chris Baker <chrisbaker@Chriss-MacBook-Pro.local>
2020-10-25 14:53:33 -0400

    Created user schema and repl file

---
+++ models/user.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    first: {
+      type: String,
+      trim: true
+    },
+    last: {
+      type: String,
+      trim: true
+    }
+  },
+
+  email: {
+    type: String,
+    required: true,
+    lowercase: true,
+    unique: true
+  },
+
+  zipCode: {
+    type: Number,
+    min: 00500,
+    max: 99950
+  },
+
+  password: {
+    type: String,
+    required: true
+  },
+
+  courses: [{ type: Schema.Types.ObjectId, ref: "Course" }],
+
+  subscribedAccount: { type: Schema.Types.ObjectId, ref: "Subscriber" }
+},
+  { timestamps: true } // Timestamp property to record createdAt and updatedAt dates
+);
+
+userSchema.virtual("fullName").get(function () {
+  return (`${this.name.first} ${this.name.last}`)
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = mongoose.model('User', userSchema);
---
+++ repl.js
@@ -0,0 +1,23 @@
+/**
+ * This file is for testing in the REPL environment.
+ * It will load this file to save time importing all the
+ * required files.
+ * 
+ * To load this file, open the REPL environment and load it
+ * 
+ *  .load repl.js
+ */
+
+const mongoose = require("mongoose");
+
+const Subscriber = require("./models/subscriber");
+
+const Course = require("./models/course");
+
+const User = require('./models/user');
+
+mongoose.connect("mongodb://localhost:27017/recipe_db", { useNewUrlParser: true});
+
+mongoose.Promise = global.Promise;
+
+
